8d05a5ca5a7b914decec3c54d9835bccb7801c30
murrutioro <101406483+murrutioro@users.noreply.github.com>
2022-03-18 14:04:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
+basic.forever(function () {
+	
+})
